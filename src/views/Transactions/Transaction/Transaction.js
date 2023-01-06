bed3a6a8f3bb118d457d85b49a174110c42aeb09
Emanuel Oliveira <emanueloliveira38@yahoo.com.br>
2023-01-06 19:15:13 -0300

    style: adding icon and changing the font of data

--- src/views/Transactions/Transaction/Transaction.js
+++ src/views/Transactions/Transaction/Transaction.js
@@ -1,27 +1,30 @@
 import React from 'react';
 
 import bindTransactionTypeIconColor from 'helpers/bindTransactionTypeIconColor';
+import useDate from 'hooks/useDate';
 
 import Icon from 'components/Icon/Icon';
 
 import styles from './Transaction.module.scss';
 
 const Transaction = ({ transaction }) => {
+  const { formatDateFromAPIToFront } = useDate();
+
   return (
     <li className={styles.transaction}>
       <div className={styles.transaction__type}>
         <Icon
           name={transaction.transaction_type?.toLowerCase()}
-          width={30}
-          height={30}
+          width={24}
+          height={24}
           fill={bindTransactionTypeIconColor(transaction.transaction_type)}
         />
       </div>
       <div className={styles.transaction__infos}>
         <div className={styles.transaction__header}>{transaction.observation}</div>
         <div className={styles.transaction__main}>
-          <div>{transaction.value}</div>
-          <div>{transaction.date}</div>
+          <div>R$ {transaction.value}</div>
+          <div>{formatDateFromAPIToFront(transaction.date)}</div>
         </div>
       </div>
     </li>
