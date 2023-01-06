ace32cc2696dadc9abfb0303678977c8f2129672
Emanuel Oliveira <emanueloliveira38@yahoo.com.br>
2023-01-06 17:40:46 -0300

    feat: adding icon to transaction

--- src/views/Transactions/Transaction/Transaction.js
+++ src/views/Transactions/Transaction/Transaction.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import bindTransactionTypeIconColor from 'helpers/bindTransactionTypeIconColor';
+
+import Icon from 'components/Icon/Icon';
+
 import styles from './Transaction.module.scss';
 
 const Transaction = ({ transaction }) => {
   return (
     <li className={styles.transaction}>
-      <div className={styles.transaction__type}>{transaction.transaction_type}</div>
+      <div className={styles.transaction__type}>
+        <Icon
+          name={transaction.transaction_type?.toLowerCase()}
+          width={30}
+          height={30}
+          fill={bindTransactionTypeIconColor(transaction.transaction_type)}
+        />
+      </div>
       <div className={styles.transaction__infos}>
         <div className={styles.transaction__header}>{transaction.observation}</div>
         <div className={styles.transaction__main}>
