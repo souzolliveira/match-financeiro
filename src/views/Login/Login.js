2f83ac713650c15c3767578adc479e6247c51a00
Emanuel Oliveira <emanueloliveira38@yahoo.com.br>
2023-01-09 20:02:25 -0300

    style: stylizing login view

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -5,6 +5,9 @@ import { useTranslation } from 'react-i18next';
 import { useAuth } from 'hooks/useAuth';
 import { useNotification } from 'hooks/useNotification';
 
+import Button from 'components/Button/Button';
+import Input from 'components/Input/Input';
+
 import styles from './Login.module.scss';
 
 const Login = () => {
@@ -28,23 +31,23 @@ const Login = () => {
   };
 
   return (
-    <div className={styles.login}>
-      <form onSubmit={e => login(e)}>
-        <div className={styles.inputGroup}>
-          <span>Email</span>
-          <input name='username' value={email} onChange={e => setEmail(e.target.value)} />
+    <form className={styles.login} onSubmit={e => login(e)}>
+      <div className={styles.login__fields}>
+        <div className={styles.login__inputgroup}>
+          <span className={styles.login__label}>{t('LOGIN.EMAIL')}</span>
+          <Input name='username' value={email} onChange={e => setEmail(e.target.value)} />
         </div>
-        <div className={styles.inputGroup}>
-          <span>Password</span>
-          <input type='password' name='password' value={password} onChange={e => setPassword(e.target.value)} />
+        <div className={styles.login__inputgroup}>
+          <span className={styles.login__label}>{t('LOGIN.PASSWORD')}</span>
+          <Input type='password' name='password' value={password} onChange={e => setPassword(e.target.value)} />
         </div>
-        <div className={styles.inputSubmit}>
-          <button type='submit' disabled={isSigningIn}>
-            Enviar
-          </button>
+        <div className={styles.login__inputsubmit}>
+          <Button type='submit' kind='primary' size='lg' disabled={isSigningIn}>
+            {t('LOGIN.SUBMIT')}
+          </Button>
         </div>
-      </form>
-    </div>
+      </div>
+    </form>
   );
 };
 
