4b86f68c43e6d3933d631d56b0d3c9f60766f973
Emanuel Oliveira <emanueloliveira38@yahoo.com.br>
2023-01-11 14:59:32 -0300

    ref: showing logo as img tag

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import Logo from 'assets/logo.png';
+
 import styles from './Header.module.scss';
 
 const Header = () => {
-  return <div className={styles.header} />;
+  return (
+    <div className={styles.header}>
+      <img alt='Match Financeiro Logo' src={Logo} className={styles.header__logo} />
+    </div>
+  );
 };
 
 export default Header;
