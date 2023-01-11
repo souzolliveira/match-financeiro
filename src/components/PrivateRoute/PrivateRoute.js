7daa2ae6b60eb1bc15323077b092fc752b5e4be3
Emanuel Oliveira <emanueloliveira38@yahoo.com.br>
2023-01-11 15:01:47 -0300

    chore: correcting app's height

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -6,17 +6,19 @@ import { useAuth } from 'hooks/useAuth';
 import Header from 'components/Header/Header';
 import Navbar from 'components/Navbar/Navbar';
 
+import styles from './PrivateRoute.module.scss';
+
 const PrivateRoute = () => {
   const { session } = useAuth();
 
   return session ? (
-    <>
+    <div className={styles.privateRoute}>
       <Header />
       <Outlet />
       <Navbar />
-    </>
+    </div>
   ) : (
-    <Navigate to='/login' />
+    <Navigate to='/match-financeiro' />
   );
 };
 
