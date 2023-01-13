2ba864ef92e67ba94bc7e3a345f30d5233d3ee6e
Emanuel Oliveira <emanueloliveira38@yahoo.com.br>
2023-01-13 19:14:46 -0300

    feat: creating Loader component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Loader.module.scss';
+
+const Loader = () => {
+  return (
+    <div className={styles.loader}>
+      <div className={styles.loader__spinner} />
+    </div>
+  );
+};
+
+export default Loader;
