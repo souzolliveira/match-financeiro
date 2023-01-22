0eca48519afce974bb818c84a4c5896c94953fa3
Emanuel Oliveira <emanueloliveira38@yahoo.com.br>
2023-01-22 15:16:47 -0300

    feat: adding button to go to the next step at the value and observation steps

--- src/components/Observation/Observation.js
+++ src/components/Observation/Observation.js
@@ -5,6 +5,8 @@ import { useTranslation } from 'react-i18next';
 import steps from 'constants/steps';
 import useHiddenStep from 'hooks/useHiddenStep';
 
+import Button from 'components/Button/Button';
+import Icon from 'components/Icon/Icon';
 import Input from 'components/Input/Input';
 
 import styles from './Observation.module.scss';
@@ -20,10 +22,14 @@ const Observation = ({ transactionObservation, setTransactionObservation, step, 
     setTransactionObservation(e.target.value);
   };
 
+  const handleNextStep = () => {
+    setStep(steps.CONFIRM);
+    setIsChangedStep(true);
+  };
+
   const handleKeyDown = e => {
     if (focused && (e.key === 'Enter' || e.key === 'Tab')) {
-      setStep(steps.CONFIRM);
-      setIsChangedStep(true);
+      handleNextStep();
     }
   };
 
@@ -38,15 +44,21 @@ const Observation = ({ transactionObservation, setTransactionObservation, step, 
     <>
       <div className={`${styles.observation} ${hidden ? styles.observation__bottom : ''} ${isChangedStep ? styles.observation__top : ''}`}>
         <span className={styles.observation__label}>{t('OBSERVATION.LABEL')}</span>
-        <Input
-          id='transaction-observation'
-          name='transaction-observation'
-          type='text'
-          value={transactionObservation}
-          onChange={e => handleChange(e)}
-          onFocus={() => setFocused(true)}
-          onBlur={() => setFocused(false)}
-        />
+        <div className={styles.observation__container}>
+          <Input
+            className={styles.observation__input}
+            id='transaction-observation'
+            name='transaction-observation'
+            type='text'
+            value={transactionObservation}
+            onChange={e => handleChange(e)}
+            onFocus={() => setFocused(true)}
+            onBlur={() => setFocused(false)}
+          />
+          <Button className={styles.observation__next} type='button' size='lg' kind='outline' onClick={() => handleNextStep()}>
+            <Icon name='arrow-right' width={24} height={24} fill='var(--gold-darkest)' />
+          </Button>
+        </div>
       </div>
       <div className={isChangedStep ? styles.observation__selected : styles.observation__unselected}>
         <span className={styles.observation__label}>{t('OBSERVATION')}</span>
--- src/components/Value/Value.js
+++ src/components/Value/Value.js
@@ -5,6 +5,8 @@ import { useTranslation } from 'react-i18next';
 import steps from 'constants/steps';
 import useHiddenStep from 'hooks/useHiddenStep';
 
+import Button from 'components/Button/Button';
+import Icon from 'components/Icon/Icon';
 import Input from 'components/Input/Input';
 
 import styles from './Value.module.scss';
@@ -22,15 +24,19 @@ const Value = ({ transactionValue, setTransactionValue, step, setStep }) => {
     }
   };
 
+  const handleNextStep = () => {
+    setStep(steps.OBSERVATION);
+    setIsChangedStep(true);
+    const valueInput = document.getElementById('transaction-observation');
+    if (valueInput) valueInput.focus();
+  };
+
   const handleKeyDown = e => {
     if (focused)
       if (e.key === 'Backspace') {
         setTransactionValue(state => (state / 10).toFixed(2));
       } else if (e.key === 'Enter' || e.key === 'Tab') {
-        setStep(steps.OBSERVATION);
-        setIsChangedStep(true);
-        const valueInput = document.getElementById('transaction-observation');
-        if (valueInput) valueInput.focus();
+        handleNextStep();
       }
   };
 
@@ -60,6 +66,16 @@ const Value = ({ transactionValue, setTransactionValue, step, setStep }) => {
             <span className={styles.value__show}>{transactionValue.replace('.', ',')}</span>
             <div className={focused ? styles.value__cursor : ''} />
           </div>
+          <Button
+            className={styles.value__next}
+            type='button'
+            size='lg'
+            kind='outline'
+            onClick={() => handleNextStep()}
+            disabled={transactionValue === (0).toFixed(2)}
+          >
+            <Icon name='arrow-right' width={24} height={24} fill='var(--gold-darkest)' />
+          </Button>
         </div>
       </div>
       <div className={isChangedStep ? styles.value__selected : styles.value__unselected}>
