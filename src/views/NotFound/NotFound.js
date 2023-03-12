c0f0ca7ae65adba2e3d4c40d66a2a8338176c5e0
Emanuel Oliveira <emanueloliveira38@yahoo.com.br>
2023-03-12 20:51:28 -0300

    feat: creating NOT FOUND screen

--- src/views/NotFound/NotFound.js
+++ src/views/NotFound/NotFound.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { useTranslation } from 'react-i18next';
+
+import Button from 'components/Button/Button';
 
 import styles from './NotFound.module.scss';
 
 const NotFound = () => {
-  return <div>404</div>;
+  const navigate = useNavigate();
+  const { t } = useTranslation();
+
+  return (
+    <div className={styles.notFound}>
+      <span className={styles.notFound__message}>{t('LOST')}</span>
+      <div className={styles.notFound__image} />
+      <Button type='button' kind='primary' size='lg' onClick={() => navigate('/')}>
+        {t('GO.HOME')}
+      </Button>
+    </div>
+  );
 };
 
 export default NotFound;
