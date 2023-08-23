30a258c2874a296f1bb377f1d2d9748f841d0a48
Emanuel Oliveira <emanueloliveira38@yahoo.com.br>
2023-08-23 16:56:40 -0300

    refactor: move the server to root

--- src/functions/firebase.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: 'AIzaSyAgFVMbARIu6vMinyFpL3oGlIa9kUvNSsk',
-  authDomain: 'match-financeiro.firebaseapp.com',
-  projectId: 'match-financeiro',
-  storageBucket: 'match-financeiro.appspot.com',
-  messagingSenderId: '375728840043',
-  appId: '1:375728840043:web:004cf732e151948640402e',
-  measurementId: 'G-JMTB9V17SS',
-};
